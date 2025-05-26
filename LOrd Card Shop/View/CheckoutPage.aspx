8141d31c5bbf924009d4b1688166996602045c66
medlynhan <midiline30@gmail.com>
2025-05-26 16:02:57 +0700

    Add Checkout Page

---
+++ LOrd Card Shop/View/CheckoutPage.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/Navbar.Master" AutoEventWireup="true" CodeBehind="CheckoutPage.aspx.cs" Inherits="LOrd_Card_Shop.View.CheckoutPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <div>
+            <h1>Checkout Page</h1>
+            <br />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:BoundField DataField="ShowName" HeaderText="Show Name" SortExpression="ShowName"></asp:BoundField>
+                    <asp:BoundField DataField="Price" HeaderText="Price" SortExpression="Price"></asp:BoundField>
+                    <asp:BoundField DataField="Description" HeaderText="Description" SortExpression="Description"></asp:BoundField>
+                    <asp:BoundField DataField="TotalPrice" HeaderText="Total Price" SortExpression="TotalPrice"></asp:BoundField>
+                </Columns>
+
+
+            </asp:GridView>
+            <br />
+
+            <asp:Label ID="Label1" runat="server" Text="Total: $ "></asp:Label><asp:Label ID="TotalPriceLbl" runat="server" Text=" "></asp:Label>
+
+            <br />
+
+            <asp:Button ID="CheckoutNowBtn" runat="server" Text="Checkout Now" />
+            
+
+
+
+        </div>
+</asp:Content>
---
+++ LOrd Card Shop/View/CheckoutPage.aspx.cs
@@ -0,0 +1,52 @@
+﻿using LOrd_Card_Shop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LOrd_Card_Shop.View
+{
+    public partial class CheckoutPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Bind();
+
+            }
+        }
+
+        private void Bind()
+        {
+            User user = (User)Session["user"];
+
+            int userId = Convert.ToInt32(user.UserId);
+
+            using (var context = new Database4Entities())
+            {
+                var cartItems = (from cart in context.Carts
+                                 join card in context.Cards on cart.CardId equals card.CardId
+                                 where cart.UserId == userId
+                                 select new
+                                 {
+                                     ShowName = card.CardName,
+                                     Price = card.CardPrice,
+                                     Description = card.CardDesc,
+                                     Quantity = cart.Quantity,
+                                     TotalPrice = cart.Quantity * card.CardPrice
+                                 }).ToList();
+
+                GridView1.DataSource = cartItems;
+                GridView1.DataBind();
+
+
+
+                decimal total = cartItems.Sum(item => item.TotalPrice);
+                TotalPriceLbl.Text = total.ToString("0.00");
+            }
+        }
+    }
+}
